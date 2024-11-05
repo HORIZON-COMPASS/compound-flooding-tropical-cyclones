ba987414ec726d6b057d7b4e9313191e7cd787b6
n-aleksandrova <natalia.aleksandrova@deltares.nl>
2024-11-05 12:34:12 +0100

    New setup of snapwave with ascii file inputs

--- SFINCS_Wave/sofala_wave/run.bat
+++
@@ -1,1 +0,0 @@
-call "C:\Intern\SFINCS_Wave\COMPASS\SFINCS_Wave\SFINCS_v2.1.1_Dollerup_release_exe\sfincs.exe">sfincs_log.txt
--- SFINCS_Wave/sofala_wave_v2/run_v2.bat
+++
@@ -1,1 +0,0 @@
-call "p:/11210471-001-compass/02_Models/00_executables/SFINCS_v2.1.1_Dollerup_release_exe/sfincs.exe">sfincs_log.txt
