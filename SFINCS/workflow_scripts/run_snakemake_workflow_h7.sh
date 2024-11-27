ddfb15583e56f24a1d9fde0e6e1324fe2fc29144
couasnonanais <couasnonanais@gmail.com>
2024-11-27 17:30:23 +0100

    updated bash script to reflect changes

--- SFINCS/workflow_scripts/run_snakemake_workflow_h7.sh
+++ SFINCS/workflow_scripts/run_snakemake_workflow_h7.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=compass-sfincs          # Job name
 #SBATCH --output=output_log_%j.log     # Standard output and error log
-#SBATCH --time=0-2:00:00           # Job duration (hh:mm:ss)
-#SBATCH --partition 16vcpu
+#SBATCH --time=0-0:15:00           # Job duration (hh:mm:ss)
+#SBATCH --partition test
 #SBATCH --exclusive 
 #SBATCH --ntasks=1                  # Number of tasks (analyses) to run
 
 module load pixi
 
 #Going to the folder where git checkout is
-#ROOT="/u/couasnon/git_repos/COMPASS/COMPASS"
-ROOT="/u/aleksand/COMPASS"
+ROOT="/u/couasnon/git_repos/COMPASS/COMPASS"
+#ROOT="/u/aleksand/COMPASS"
 cd "${ROOT}"
 
 # Installing pixi environment
@@ -22,7 +22,7 @@ source hook.sh
 cd SFINCS/workflow_scripts
 
 #Unlocking the directory for snakemake
-snakemake --unlock -s snakefile_dict --configfile config/config_general.yml 
+snakemake --unlock -s snakefile --configfile config_snakemake/config_general.yml 
 
 # running workflow with snakemake
 snakemake -s snakefile --configfile config_snakemake/config_general.yml --forceall --rulegraph | dot -Tpdf > dag.pdf
