29998c921f8a2568644dfc401ad2c33aab21f752
Tycho Bovenschen <tycho.bovenschen@deltares.nl>
2024-11-28 13:21:52 +0100

    Add wflow-sfincs coupling

---
+++ Wflow/update_forcing_wflow_event.py
@@ -0,0 +1,67 @@
+# %%
+from os.path import basename, join
+
+import pandas as pd
+from hydromt.config import configread
+from hydromt.log import setuplog
+from hydromt_wflow import WflowModel
+
+# %%
+# model and data paths/
+logger = setuplog("update", "./hydromt.log", log_level=10)
+# wflow_root = r"p:\11208614-de-370a\01_models\Humber\wflow"
+if "snakemake" in locals():
+    script_root = snakemake.params.script_root
+    wflow_root = snakemake.params.wflow_root
+    use_case = snakemake.params.use_case
+    start_time = snakemake.params.start_time
+    end_time = snakemake.params.end_time
+    event = snakemake.params.event
+    meteo_fn = snakemake.params.meteo_fn
+    meteo_option = snakemake.params.meteo_option
+else:
+    script_root = r"p:\11208614-de-370a\02_scripts\wflow_sfincs_snake"
+    wflow_root = r"p:\11208614-de-370a\01_models\Reunion\wflow"
+    use_case = "Reunion"
+    start_time = "2024-01-12T00:00:00"
+    end_time = "2024-01-19T00:00:00"
+    event = "2024-01-12"
+    meteo_fn = "era5_hourly_local"
+    meteo_option = "ERA5"
+
+# %% Setup forcing
+
+mod = WflowModel(
+    root=wflow_root,
+    data_libs=["deltares_data", r"p:\11208614-de-370a\01_models\local_data.yml"],
+    mode="r",
+    logger=logger,
+)
+mod.read()
+
+opt = {
+    "setup_config": {
+        "starttime": start_time,
+        "endtime": end_time,
+        "timestepsecs": 3600,
+        "model.reinit": False,
+        "input.path_static": "..\..\..\staticmaps.nc",
+    },
+    "setup_precip_forcing": {
+        "precip_fn": meteo_fn,
+        "precip_clim_fn": None,
+    },
+    "setup_temp_pet_forcing": {
+        "temp_pet_fn": meteo_fn,
+        "press_correction": True,
+        "temp_correction": True,
+        "pet_method": "debruin",
+        "skip_pet": False,
+    },
+}
+
+mod.set_root(join(wflow_root, "events", event, meteo_option), mode="w+")
+mod.update(opt=opt, write=False)
+mod.write_forcing(fn_out=join(mod.root, "inmaps.nc"))
+mod.write_config()
+# %%
