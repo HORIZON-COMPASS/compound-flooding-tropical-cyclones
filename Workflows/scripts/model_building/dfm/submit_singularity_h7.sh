123b4a27fe108cf944bd5e39a2fbee8f92824829
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-01-15 14:39:39 +0100

    updated singularity script tested on H7

--- Workflows/scripts/model_building/dfm/submit_singularity_h7.sh
+++ Workflows/scripts/model_building/dfm/submit_singularity_h7.sh
@@ -1,44 +1,88 @@
 #! /bin/bash
+ 
+# This is a script for submitting single or multi-node simulations to the Slurm cluster at Deltares (H7).
+# Note: Apptainer is the replacement for Singularity.
 
-# Usage: 
-#   - Place this script in the same folder as the model
-#   - Modify this script where needed (e.g. number of nodes, number of tasks per node, singularity version, model folder)
-#   - Execute this script using:
-#     sbatch ./submit_singularity_h7_mv.sh
+# Usage:
+#   - Modify this script where needed (e.g. number of nodes, number of tasks per node, Apptainer version).
+#   - Execute this script from the command line of H7 using:
+#     sbatch ./submit_singularity_h7.sh
 #
-# This is an h7 specific script for single- or multi-node simulations
 
-#SBATCH --nodes=1               #-N, -n is total numer of nodes. $SLURM_NTASKS = "--nodes" times "--ntasks-per-node"
-#SBATCH --ntasks-per-node=4     #you pay for a minimum of 1/4 of the cores on a node
-#SBATCH --job-name=JOBNAME     #-J
-#SBATCH --time 1-00:00:00        #-t, reduce the expected time if possible to increase your priority
-#SBATCH --partition=4vcpu      #Partition name
+ 
+#--- Specify Slurm SBATCH directives ------------------------------------------------------------------------
+#SBATCH --nodes=1               # Number of nodes.
+#SBATCH --ntasks-per-node=1     # The number of tasks to be invoked on each node.
+                                # For sequential runs, the number of tasks should be '1'.
+                                # Note: SLURM_NTASKS is equal to "--nodes" multiplied by "--ntasks-per-node".
+#SBATCH --job-name=JOBNAME      # Specify a name for the job allocation.
+#SBATCH --time 00:30:00         # Set a limit on the total run time of the job allocation.
+#SBATCH --partition=test        # Request a specific partition for the resource allocation.
+                                # See: https://publicwiki.deltares.nl/display/Deltareken/Compute+nodes.
+##SBATCH --exclusive            # The job allocation can not share nodes with other running jobs.
+                                # In many cases this option can be omitted.
+##SBATCH --contiguous           # The allocated nodes must form a contiguous set, i.e. next to each other.
+                                # In many cases this option can be omitted.
+ 
+#--- Load modules (for use within Deltares) ------------------------------------------------------------------
+module purge
+module load apptainer/1.2.5     # Load the Apptainer container system software.
+module load intelmpi/2021.11.0   # Load the  message-passing library for parallel simulations.
+ 
+ 
+#--- Setup the container ------------------------------------------------------------------------------------
+# For use within Deltares, Delft3D FM Apptainer containers are available here: P:\d-hydro\delft3dfm_containers\
+# Specify the folder that contains the required version of the Apptainer container
+containerFolder=/p/d-hydro/delft3dfm_containers/delft3dfm_2025.01/
+ 
+ 
+#--- Setup the model ----------------------------------------------------------------------------------------
+# Specify the ROOT folder of your model, i.e. the folder that contains ALL of the input files and sub-folders, e.g:
+modelFolder=MDUFOLDER
 
-#---You will need to modify the input below this line---
+# Specify the folder containing your model's MDU file.
+mdufileFolder=$modelFolder/
+ 
+# Specify the folder containing your DIMR configuration file.
+dimrconfigFolder=$modelFolder
+ 
+# The name of the DIMR configuration file. The default name is dimr_config.xml. This file must already exist!
+dimrFile=dimr_config.xml
+ 
+# This setting might help to prevent errors due to temporary locking of NetCDF files. 
+export HDF5_USE_FILE_LOCKING=FALSE
 
-#Set MDU file
-mduFile=MDUFILE
 
-#---You do not need to modify anything below this line---
-
-# Set the location of the Singularity container.
-export singularityFolder=/p/11210471-001-compass/02_Models/00_executables/Apptainer_DFLOWFM_2023_02_copy/
-
-#
-#
-# --- You shouldn't need to change the lines below ------------------------
-
-# stop after an error occurred:
+# Stop the computation after an error occurs.
 set -e
-nPart=$SLURM_NTASKS*$SLURM_NNODES
-echo ""
-echo "nPart" $nPart
-
-echo ""
-echo "Partitioning..."
-echo "SLURM_NTASKS: $SLURM_NTASKS"
-srun -n 1 $singularityFolder/execute_singularity_h7.sh -p 0 run_dflowfm.sh --partition:ndomains=$SLURM_NTASKS:icgsolver=6 $mduFile
-
+ 
+# For parallel processes, the lines below update the <process> element in the DIMR configuration file.
+# The updated list of numbered partitions is calculated from the user specified number of nodes and cores.
+# You DO NOT need to modify the lines below.
+PROCESSSTR="$(seq -s " " 0 $((SLURM_NTASKS-1)))"
+sed -i "s/\(<process.*>\)[^<>]*\(<\/process.*\)/\1$PROCESSSTR\2/" $dimrconfigFolder/$dimrFile
+ 
+# The name of the MDU file is read from the DIMR configuration file.
+# You DO NOT need to modify the line below.
+mduFile="$(sed -n 's/\r//; s/<inputFile>\(.*\).mdu<\/inputFile>/\1/p' $dimrconfigFolder/$dimrFile)".mdu
+ 
+ 
+#--- Partition by calling the dflowfm executable -------------------------------------------------------------
+if [ "$SLURM_NTASKS" -gt 1 ]; then 
+    echo ""
+    echo "Partitioning parallel model..."
+    cd "$mdufileFolder"
+    echo "Partitioning in folder ${PWD}"
+    srun -n 1 -N 1 $containerFolder/execute_singularity_h7.sh -c $containerFolder -m $modelFolder dflowfm --nodisplay --autostartstop --partition:ndomains="$SLURM_NTASKS":icgsolver=6 "$mduFile"
+else 
+    #--- No partitioning ---
+    echo ""
+    echo "Sequential model..."
+fi 
+ 
+#--- Simulation by calling the dimr executable ----------------------------------------------------------------
 echo ""
 echo "Simulation..."
-srun $singularityFolder/execute_singularity_h7.sh -p 0 run_dflowfm.sh $mduFile
+cd $dimrconfigFolder
+echo "Computing in folder ${PWD}"
+srun $containerFolder/execute_singularity_h7.sh -c $containerFolder -m $modelFolder dimr "$dimrFile"
