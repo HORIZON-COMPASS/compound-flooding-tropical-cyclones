a0b30083fe24058694f0c43de4535d0338e55804
Natalia Aleksandrova <aleksand@v-slurmsub001.DIRECTORY.INTRA>
2025-01-27 14:46:01 +0100

    batch file edits to include new pixi environment name

--- Workflows/run_snakemake_dfm_workflow_h7.sh
+++ Workflows/run_snakemake_dfm_workflow_h7.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=compass-dfm         # Job name
 #SBATCH --output=output_log_%j.log     # Standard output and error log
-#SBATCH --time=0-0:30:00           # Job duration (hh:mm:ss)
-#SBATCH --partition test
+#SBATCH --time=0-3:00:00           # Job duration (hh:mm:ss)
+#SBATCH --partition 16vcpu
 #SBATCH --exclusive 
 #SBATCH --ntasks=1                  # Number of tasks (analyses) to run
 
@@ -11,22 +11,23 @@ module load pixi
 #Going to the folder where git checkout is
 #ROOT="/u/couasnon/git_repos/COMPASS/COMPASS"
 # ROOT="/u/bovensch/git_repos/COMPASS"
-ROOT="/u/vertegaa/git_repos/COMPASS"
+#ROOT="/u/vertegaa/git_repos/COMPASS"
+ROOT="/u/aleksand/compound-flooding-tropical-cyclones/"
 cd "${ROOT}"
 
 # Installing pixi environment
-pixi install --environment compass-dfm
-pixi shell-hook --environment compass-dfm > hook.sh
+pixi install --environment compass-snake-dfm
+pixi shell-hook --environment compass-snake-dfm > hook.sh
 source hook.sh
 
 # Navigate to directory where the scripts are
 cd Workflows
 
 #Unlocking the directory for snakemake
-snakemake --unlock -s snakefile_dfm.smk --configfile config_snakemake/config_general_dfm.yml 
+snakemake --unlock -s snakefile_dfm.smk --configfile config_snakemake/config_general.yml 
 
 # running workflow with snakemake
-snakemake -s snakefile_dfm.smk --configfile config_snakemake/config_general_dfm.yml --forceall --rulegraph | dot -Tpdf > dag_dfm.pdf
-snakemake -s snakefile_dfm.smk --configfile config_snakemake/config_general_dfm.yml --cores 'all' --latency-wait 60 --wait-for-files  --forceall # --cores 4
+snakemake -s snakefile_dfm.smk --configfile config_snakemake/config_general.yml --forceall --rulegraph | dot -Tpdf > dag_dfm.pdf
+snakemake -s snakefile_dfm.smk --configfile config_snakemake/config_general.yml --cores 'all' --latency-wait 60 --wait-for-files  # --forceall # --cores 4
 
 exit
