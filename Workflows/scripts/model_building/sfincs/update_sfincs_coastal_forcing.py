1a5b8938a72a69dfe7d08788292759a06f59301b
Natalia Aleksandrova <natalia.aleksandrova@deltares.nl>
2025-01-29 16:24:14 +0100

    moved sfincs and wflow scripts to model_building folder

---
+++ Workflows/scripts/model_building/sfincs/update_sfincs_coastal_forcing.py
@@ -0,0 +1,72 @@
+# %%
+from datetime import datetime as datetime
+
+from hydromt.config import configread
+from hydromt.log import setuplog
+from hydromt import data_catalog
+import shutil
+import os
+from os.path import join
+from hydromt_sfincs import SfincsModel
+
+# %%
+# model and data paths/
+logger = setuplog("update", "./hydromt.log", log_level=10)
+if "snakemake" in locals():
+    sfincs_mod_no_forcing = snakemake.params.dir_run_no_forcing
+    sfincs_mod_with_forcing = snakemake.params.dir_run_with_forcing
+    data_cats = snakemake.params.data_cats
+    wind_forcing = snakemake.params.wind_forcing
+    start_time = snakemake.params.start_time
+    end_time = snakemake.params.end_time
+    precip_forcing = snakemake.wildcards.forcing
+    dfm_output = snakemake.params.dfm_output
+    utmzone = snakemake.params.utmzone
+
+data_catalog = data_catalog.DataCatalog(data_cats)
+
+#%% Create a configuration
+opt = {
+    'setup_config': {
+        'dtout': 3600,
+        'dthisout': 3600,
+        'storemeteo': 1,
+        'utmzone':utmzone
+        }
+}
+
+#  Set starttime and endtime
+opt["setup_config"]["tref"] = start_time
+opt["setup_config"]["tstart"] = start_time
+opt["setup_config"]["tstop"] = end_time
+
+# Add rainfall forcing
+opt['setup_precip_forcing_from_grid'] = dict(precip=precip_forcing,aggregate=False)
+
+# Add coastal water level forcing
+opt['setup_waterlevel_forcing'] = dict(geodataset=dfm_output,buffer=1000,merge=False)
+
+#%%
+mod = SfincsModel(
+    root=sfincs_mod_no_forcing,
+    data_libs=data_cats,
+    mode="r",
+    logger=logger,
+)
+
+if 'spw' in wind_forcing:
+    meteo_type = 'spiderweb'
+    spw = 1 
+    spw_input = data_catalog[wind_forcing].path
+    spw_file = os.path.basename(spw_input)
+    spw_copy = os.path.join(sfincs_mod_with_forcing,spw_file)
+    shutil.copyfile(spw_input, spw_copy)
+    opt["setup_config"]["spwfile"] =  os.path.basename(spw_file)
+# Add other specifications for when the wind fields are prescribed via another format
+
+mod.update(
+    model_out = sfincs_mod_with_forcing,
+    write=True,
+    forceful_overwrite=True,
+    opt=opt
+)
