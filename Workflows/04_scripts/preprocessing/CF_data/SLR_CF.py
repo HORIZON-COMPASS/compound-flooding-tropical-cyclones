4277d36d9efa0080402aed5cf758013803dd55d2
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-03-17 21:18:39 +0100

    Adding extra model to compare SLR

--- Workflows/04_scripts/preprocessing/CF_data/SLR_CF.py
+++ Workflows/04_scripts/preprocessing/CF_data/SLR_CF.py
@@ -19,7 +19,6 @@ start_date = np.datetime64('2019-03-09T00:00')
 end_date = np.datetime64('2019-03-24T00:00') 
 #%% Reading in ISIMIP data
 cat = hydromt.DataCatalog("../../../03_data_catalogs/datacatalog_SFINCS_coastal_coupling.yml")
-cat = hydromt.DataCatalog("../../../03_data_catalogs/datacatalog_CF_forcing.yml")
 # %%
 # dfm_idai = cat.get_dataset("dfm_output_MZB_Idai")
 dfm_Idai_CF0 = cat.get_dataset("dfm_output_event_450_gebco2024_MZB_GTSMv41opendap_CF0_spw_IBTrACS_CF0")
@@ -34,27 +33,11 @@ dfm_Idai_CF0_copy.to_netcdf(outfile_path + "dfm_output_event_450_gebco2024_MZB_G
 dfm_Idai_CF0_copy.close()
 
 # %%
-dfm_idai = cat.get_dataset("dfm_output_MZB_Idai")
-dfm_idai_CF = dfm_idai.copy()
-
-# %%
-dfm_idai_CF['waterlevel'] = dfm_idai['waterlevel'] - 0.141
-# %% Export the counterfactual ds
-outfile_path = "p:/11210471-001-compass/01_Data/counterfactuals/SLR/ISIMIP/"
-
-dfm_idai_CF.to_netcdf(outfile_path + "dfm_output_MZB_Idai_SLR2015.his")
-dfm_idai_CF.close()
-
-# %%
 # Plot the mean data
 # Set up the subplots
 fig, ax = plt.subplots(figsize=(15, 10))
 # mean_data_F.plot(ax=ax)
-<<<<<<< HEAD
-dfm_Idai_CF0_copy['waterlevel'].plot(ax=ax)
-=======
 dfm_idai_CF['waterlevel'].plot(ax=ax)
->>>>>>> 6064949 (adding SLR processing scripts)
 
 # %% Select water level data for one station
 # Create the plot
