9c4c7eb4d5c268e2c9003096ca0497014d04470a
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-04-02 09:49:44 +0200

    adding documentation

--- Workflows/04_scripts/model_building/wflow/setup_wflow_base.py
+++ Workflows/04_scripts/model_building/wflow/setup_wflow_base.py
@@ -1,39 +1,41 @@
 # %%
-from os.path import basename, join, exists
-
+from os.path import join, exists
 import os
 from hydromt.config import configread
 from hydromt.log import setuplog
 from hydromt_wflow import WflowModel
 import geopandas as gpd
+
 # %%
 if "snakemake" in locals():
-    model_dir = snakemake.params.dir_model
-    config_file = snakemake.input.config_file
-    data_cat = snakemake.params.data_cat
-    region_geom = snakemake.input.region_geom
+    model_dir        = snakemake.params.dir_model
+    config_file      = snakemake.input.config_file
+    data_cat         = snakemake.params.data_cat
+    region_geom      = snakemake.input.region_geom
     dir_sfincs_model = snakemake.input.dir_sfincs_model
 else:
-    model_dir = "p:/11210471-001-compass/02_Models/sofala/Idai/wflow_test"
-    config_file = "../../../05_config_models/01_wflow/config_wflow.yml"
-    data_cat = [
+    model_dir        = "p:/11210471-001-compass/02_Models/sofala/Idai/wflow_test"
+    config_file      = "../../../05_config_models/01_wflow/config_wflow.yml"
+    data_cat         = [
         '../../../03_data_catalogs/datacatalog_general.yml',
-        '../../../03_data_catalogs/datacatalog_CF_forcing.yml',
-    ] 
-    region_geom = 'p:/11210471-001-compass/02_Models/sofala/Idai/sfincs_test/gis/region.geojson'
+        '../../../03_data_catalogs/datacatalog_CF_forcing.yml'
+        ] 
+    region_geom      = 'p:/11210471-001-compass/02_Models/sofala/Idai/sfincs_test/gis/region.geojson'
     dir_sfincs_model = 'p:/11210471-001-compass/02_Models/sofala/Idai/sfincs_test'
 
 #%%
+# Check whether model folder exists
 if not exists(model_dir):
     os.mkdir(model_dir)
+
 # model and data paths/
 logger = setuplog("update", join(model_dir, "hydromt.log"), log_level=10)
 
-
-
-opt = configread(config_file, abs_path=True)  # read settings from ini file
+# read settings from ini file
+opt = configread(config_file, abs_path=True)  
 kwargs = opt.pop("global", {})
 
+# Set up output points based on SFINCS inflow river points
 opt['setup_gauges'] = {
     'gauges_fn': join(dir_sfincs_model, "gis", "src.geojson"),
     'snap_to_river': True,
@@ -45,11 +47,11 @@ opt['setup_gauges'] = {
 }
 
 #%%
-
+# Read SFINCS region
 region = gpd.read_file(region_geom).to_crs(epsg = '4326')
 
-
 #%%
+# Set up wflow 
 mod = WflowModel(
     root=model_dir, data_libs=data_cat, mode="w+", logger=logger, **kwargs
 )
