f4f5eff46a6140ce2b910b1fe84d72f535d0a239
dvertegaal <doris.vertegaal@deltares.nl>
2025-05-22 12:48:38 +0200

    30yr wflow run to determine qbankful

---
+++ Workflows/00_execution_script_examples/run_snakemake_wflow30yr_workflow_h7.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=compass-wflow         # Job name
+#SBATCH --output=00_execution_script_examples/logs/slurm/slurm_wflow_%j.log     # Standard output and error log
+#SBATCH --time=0-0:30:00           # Job duration (hh:mm:ss)
+#SBATCH --partition test    # 16vcpu
+#SBATCH --exclusive 
+#SBATCH --ntasks=1                  # Number of tasks (analyses) to run
+
+module load pixi
+module load julia
+
+#Going to the folder where git checkout is
+#ROOT="/u/couasnon/git_repos/COMPASS/COMPASS"
+#ROOT="/u/bovensch/git_repos/COMPASS"
+ROOT="/u/vertegaa/git_repos/COMPASS"
+# ROOT="/u/aleksand/compound-flooding-tropical-cyclones/"
+cd "${ROOT}"
+
+# Installing pixi environment
+pixi install --environment compass-wflow
+pixi shell-hook --environment compass-wflow > hook.sh
+source hook.sh
+
+# Install Julia environment
+julia +1.9 -e 'using Pkg; Pkg.instantiate(); Pkg.add("Wflow")'
+
+# Navigate to directory where the scripts are
+cd Workflows/02_workflow_rules
+
+#Unlocking the directory for snakemake
+snakemake --unlock -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml 
+
+# running workflow with snakemake
+snakemake -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml --forceall --rulegraph | dot -Tpdf > wflow_30yr.pdf
+snakemake -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml --cores 'all' --latency-wait 60 --wait-for-files --rerun-incomplete # --cores 4
+
+exit
