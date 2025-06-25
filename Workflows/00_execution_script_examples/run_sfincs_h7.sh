5e5fec129fddb2bdd191ce24f5e6febfd5f0466d
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-06-25 09:33:00 +0200

    sfincs only run batch file

---
+++ Workflows/00_execution_script_examples/run_sfincs_h7.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=compass-sfincs
+#SBATCH --output=output_log_%j.log
+#SBATCH --time=0-1:00:00
+#SBATCH --partition=4vcpu
+#SBATCH --nodes=1                    # Adjust as needed
+#SBATCH --ntasks-per-node=4          # Match Snakemake's `taskspernode`
+#SBATCH --exclusive
+
+# Run SFINCS using Docker
+echo "Running SFINCS model using Docker..."
+
+RUN_DIR="/p/11210471-001-compass/03_Runs/sofala/Idai/sfincs/event_tp_era5_hourly_CF0_GTSMv41_CF0_spw_IBTrACS_CF0_waves"
+
+echo "Working directory: ${RUN_DIR}"
+
+docker run --rm \
+    --mount type=bind,src="${RUN_DIR}",target=/data \
+    deltares/sfincs-cpu:latest sfincs
+
+echo "Finished running SFINCS"
