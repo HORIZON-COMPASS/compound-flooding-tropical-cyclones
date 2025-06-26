9196f354d62aac51e67c0be9d13eaae923548bd4
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-06-26 11:51:49 +0200

    use discharge with bankful removed

--- Workflows/04_scripts/model_building/sfincs/update_sfincs_dis_forcing.py
+++ Workflows/04_scripts/model_building/sfincs/update_sfincs_dis_forcing.py
@@ -18,14 +18,14 @@ else:
     curdir              = '../../../'
     region              = "sofala"
     tc_name             = "Idai"
-    precip_forcing      = "era5_hourly"
+    precip_forcing      = "era5_hourly_zarr"
     wind_forcing        = 'spw_IBTrACS'
     tidemodel           = 'GTSMv41' # tidemodel: FES2014, FES2012, EOT20, GTSMv4.1, GTSMv4.1_opendap, tpxo80_opendap
     CF_rain_txt         = "0"
     CF_SLR_txt          = "0"
     CF_wind_txt         = "0"
     wflow_root          = f"p:/11210471-001-compass/03_Runs/{region}/{tc_name}/wflow/event_precip_{precip_forcing}_CF{CF_rain_txt}"
-    sfincs_model_folder = f"p:/11210471-001-compass/03_Runs/{region}/{tc_name}/sfincs/event_tp_{precip_forcing}_CF{CF_rain_txt}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}_waves"
+    sfincs_model_folder = f"p:/11210471-001-compass/03_Runs/{region}/{tc_name}/sfincs/event_tp_{precip_forcing}_CF{CF_rain_txt}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}"
     data_cats           = [
         join(curdir, "03_data_catalogs", "datacatalog_general.yml"), 
         join(curdir, "03_data_catalogs", "datacatalog_SFINCS_coastal_coupling.yml"), 
@@ -35,35 +35,25 @@ else:
     wflow_dis_no_bankfull = f"{wflow_root}/events/run_default/wflow_dis_no_qbankfull.csv"
 
 #%%
-mod = WflowModel(
-    root=join(wflow_root, 'events'),
-    data_libs=data_cats,
-    mode="r",
-    logger=logger,
-)
-mod.read()
-
 # Read config rile from sfincs model with coastal and meteo forcing
 inp = SfincsInput.from_file(join(sfincs_model_folder,"sfincs.inp"))
 config = inp.to_dict()
+
 #%%
 # Write dis file to sfincs event model folder
 reftime_object = config["tref"]
-df = mod.results['netcdf']['Q'].to_pandas()
+
+# Read wflow qbankfull corrected discharge
+df = pd.read_csv(wflow_dis_no_bankfull)
+df.set_index('time', inplace=True)
+df.columns.name = "Q_gauges_locs"
+df.index = pd.to_datetime(df.index)
 df.index = (df.index - reftime_object).total_seconds()
 df.to_csv(
     join(sfincs_model_folder, "sfincs.dis"),
     sep=" ",
     header=False,
 )
-# # Read wflow qbankfull corrected discharge
-# df = pd.read_csv(wflow_dis_no_bankfull)
-# df.index = (df.index - reftime_object).total_seconds()
-# df.to_csv(
-#     join(sfincs_model_folder, "sfincs.dis"),
-#     sep=" ",
-#     header=False,
-# )
 
 config.update({"disfile": "sfincs.dis"})
 config.update({"srcfile": "sfincs.src"})
