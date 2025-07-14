a49aa459ab37f145a5ab6098bcd9823462e3a93f
dvertegaal <doris.vertegaal@deltares.nl>
2025-07-14 14:06:59 +0200

    debug after H7 update

--- Workflows/00_execution_script_examples/run_snakemake_wflow30yr_workflow_h7.sh
+++ Workflows/00_execution_script_examples/run_snakemake_wflow30yr_workflow_h7.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=compass-wflow         # Job name
 #SBATCH --output=00_execution_script_examples/logs/slurm/slurm_wflow_%j.log     # Standard output and error log
-#SBATCH --time=0-0:30:00           # Job duration (hh:mm:ss)
-#SBATCH --partition test    # 16vcpu
+#SBATCH --time=0-4:00:00           # Job duration (hh:mm:ss)
+#SBATCH --partition 16vcpu
 #SBATCH --exclusive 
 #SBATCH --ntasks=1                  # Number of tasks (analyses) to run
+
+export PIXI_CACHE_DIR=/tmp/$USER/pixi-cache
+mkdir -p "$PIXI_CACHE_DIR"
 
 module load pixi
 module load julia
@@ -16,22 +19,27 @@ ROOT="/u/vertegaa/git_repos/COMPASS"
 # ROOT="/u/aleksand/compound-flooding-tropical-cyclones/"
 cd "${ROOT}"
 
-# Installing pixi environment
+# Install pixi environment if not already installed
+echo "Installing pixi environment..."
 pixi install --environment compass-wflow
-pixi shell-hook --environment compass-wflow > hook.sh
-source hook.sh
+
+# Activate pixi environment
+echo "Activating pixi shell environment..."
+eval "$(pixi shell-hook --environment compass-wflow)"
 
 # Install Julia environment
+echo "Setting up Julia environment..."
 julia +1.9 -e 'using Pkg; Pkg.instantiate(); Pkg.add("Wflow")'
 
 # Navigate to directory where the scripts are
+echo "Changing to workflows directory..."
 cd Workflows/02_workflow_rules
 
 #Unlocking the directory for snakemake
 snakemake --unlock -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml 
 
 # running workflow with snakemake
-snakemake -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml --forceall --rulegraph | dot -Tpdf > wflow_30yr.pdf
+# snakemake -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml --forceall --rulegraph | dot -Tpdf > wflow_30yr.pdf
 snakemake -s snakefile_wflow_30yr.smk --configfile ../01_config_snakemake/config_general_MZB.yml --cores 'all' --latency-wait 60 --wait-for-files --rerun-incomplete # --cores 4
 
 exit
