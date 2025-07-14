29345aabcb6701eebaaf6d27181e9b683626e8f1
dvertegaal <doris.vertegaal@deltares.nl>
2025-07-14 14:01:12 +0200

    load modules

--- Workflows/00_execution_script_examples/run_dfm_h7.sh
+++ Workflows/00_execution_script_examples/run_dfm_h7.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=compass-dfm
-#SBATCH --output=output_log_%j.log
-#SBATCH --time=0-4:00:00
-#SBATCH --partition=16vcpu
+#SBATCH --job-name=compass-dfm          # Job name
+#SBATCH --output=00_execution_script_examples/logs/slurm/slurm_dfm_%j.log     # Standard output and error log
+#SBATCH --time=0-4:00:00           # Job duration (hh:mm:ss)
+#SBATCH --partition 16vcpu
 #SBATCH --nodes=1                    # Adjust as needed
-#SBATCH --ntasks-per-node=16        # Match Snakemake's `taskspernode`
-#SBATCH --exclusive
+#SBATCH --ntasks-per-node=16    
+#SBATCH --exclusive 
 
+module load apptainer/1.2.5     # Load the Apptainer container system software.
+module load intelmpi/2021.11.0   # Load the  message-passing library for parallel simulations.
+ 
 set -euo pipefail
 
 echo "Running DFM model manually..."
 
-EVENT_DIR="/p/11210471-001-compass/03_Runs/sofala/Idai/dfm/event_450_gebco2024_MZB_GTSMv41_CF0_era5_hourly_zarr_CF0"
+EVENT_DIR="/p/11210471-001-compass/03_Runs/sofala/Idai/dfm/event_450_gebco2024_MZB_GTSMv41_CF0_spw_IBTrACS_CF0"
 SCRIPT="${EVENT_DIR}/run_singularity_h7.sh"
 
 # Optional: copy script and make it executable if needed
