66e8dc596cfc02855584ce339970267bae2f3925
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-09-02 10:36:14 +0200

    separate script for supplementary fig

---
+++ Attribution_results/scripts/A_SFINCS_basemap.py
@@ -0,0 +1,49 @@
+#%%
+# Load modules
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+from os.path import join
+from hydromt_sfincs import SfincsModel, utils
+
+region               = "sofala"
+tc_name              = "Idai"
+wind_forcing         = 'era5_hourly_spw_IBTrACS'
+precip_forcing       = 'era5_hourly_zarr'
+tidemodel            = 'GTSMv41' # tidemodel: FES2014, FES2012, EOT20, GTSMv4.1, GTSMv4.1_opendap, tpxo80_opendap
+datacat              = [
+    '../../Workflows/03_data_catalogs/datacatalog_general.yml',
+    '../../Workflows/03_data_catalogs/datacatalog_SFINCS_obspoints.yml',
+    '../../Workflows/03_data_catalogs/datacatalog_SFINCS_coastal_coupling.yml',
+    '../../Workflows/03_data_catalogs/datacatalog_CF_forcing.yml'
+    ]
+CF_SLR_txt           = "0"
+CF_wind_txt          = "0"
+CF_rain_txt          = "0"
+
+# Factual model
+model_name           = f"event_tp_{precip_forcing}_CF{CF_rain_txt}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}"
+dir_run              = f"p:/11210471-001-compass/03_runs/{region}/{tc_name}/sfincs/{model_name}"
+outfile_png          = f"../figures/fS1.png"
+outfile_pdf          = f"../figures/fS1.pdf"
+
+# select the model and datacatalog
+sfincs_root = dir_run
+mod = SfincsModel(sfincs_root, data_libs=datacat, mode="r")
+
+# reading in the model results
+mod.read_results()
+
+# Save as png
+fig, ax = mod.plot_basemap(
+    fn_out=os.path.join(os.path.abspath(os.path.dirname(outfile_png)),os.path.basename(outfile_png)), 
+    plot_geoms=True, 
+    figsize=(8, 6))
+
+# And as pdf
+fig, ax = mod.plot_basemap(
+    fn_out=os.path.join(os.path.abspath(os.path.dirname(outfile_pdf)),os.path.basename(outfile_pdf)), 
+    plot_geoms=True, 
+    figsize=(8, 6))
+# %%
