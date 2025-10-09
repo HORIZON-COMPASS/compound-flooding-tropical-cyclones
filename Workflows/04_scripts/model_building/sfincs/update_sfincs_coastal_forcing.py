b81622fb88ef1f8f608035425c94c4f211921496
Doris Vertegaal <161464896+dvertegaal@users.noreply.github.com>
2025-10-09 14:24:31 +0200

    update for velocity test

--- Workflows/04_scripts/model_building/sfincs/update_sfincs_coastal_forcing.py
+++ Workflows/04_scripts/model_building/sfincs/update_sfincs_coastal_forcing.py
@@ -34,8 +34,8 @@ else:
     region                  = "sofala"
     utmzone                 = '36s'
     tc_name                 = "Idai"
-    wind_forcing            = 'spw_IBTrACS'
-    precip_forcing          = 'era5_hourly'
+    wind_forcing            = 'era5_hourly_spw_IBTrACS'
+    precip_forcing          = 'era5_hourly_zarr'
     dfm_res                 = "450"
     bathy                   = "gebco2024_MZB"
     tidemodel               = 'GTSMv41' # tidemodel: FES2014, FES2012, EOT20, GTSMv4.1, GTSMv4.1_opendap, tpxo80_opendap
@@ -50,13 +50,13 @@ else:
     CF_SLR_txt              = "0"
     CF_wind_txt             = "0"
     start_time              = '20190309 000000'
-    end_time                = '20190325 060000'
+    end_time                = '20190325 000000'
     use_dfm                 = True
     use_waves               = True
     dfm_model               = f"event_{dfm_res}_{bathy}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}"
     dfm_output              = f"dfm_output_{dfm_model}"
-    sfincs_mod_no_forcing   = os.path.join(f"p:/11210471-001-compass/02_Models/{region}/{tc_name}/sfincs_test")
-    sfincs_mod_with_forcing = os.path.join(f"p:/11210471-001-compass/03_Runs/{region}/{tc_name}/sfincs/event_tp_{precip_forcing}_CF{CF_rain_txt}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}_waves")
+    sfincs_mod_no_forcing   = os.path.join(f"p:/11210471-001-compass/02_Models/{region}/{tc_name}/sfincs")
+    sfincs_mod_with_forcing = os.path.join(f"p:/11210471-001-compass/03_Runs/{region}/{tc_name}/sfincs/event_tp_{precip_forcing}_CF{CF_rain_txt}_{tidemodel}_CF{CF_SLR_txt}_{wind_forcing}_CF{CF_wind_txt}_maxvel")
     obs_points              = os.path.join("p:/11210471-001-compass/01_Data/sfincs_obs_points/obs_locs_sofala.geojson")
 
 #%%
